b39051f03797e3f0ee7dd89926689addc8076d31
tapan0810 <arceus0810@gmail.com>
2026-02-17 08:33:44 +0530

    Add project files.

---
+++ Dotnet_API_14_API_2_API-call/Data/HotelDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet_API_14_API_2_API_call.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_14_API_2_API_call.Data
+{
+    public class HotelDbContext:DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ Dotnet_API_14_API_2_API-call/Entities/Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dotnet_API_14_API_2_API_call.Entities.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string HotelName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
